3e02dda7b5c20d7cf69327d1cbb963cb82096090
Luiz Carlos Mourão Paes de Carvalho <luizcarlosmpc@gmail.com>
2020-05-13 02:02:25 -0300

    Added abstract implementation, removed gradle files

---
+++ src/main/java/me/saiintbrisson/bukkit/boilerplate/common/AbstractCacheStore.java
@@ -0,0 +1,56 @@
+package me.saiintbrisson.bukkit.boilerplate.common;
+
+import me.saiintbrisson.bukkit.boilerplate.CacheStore;
+import me.saiintbrisson.bukkit.boilerplate.Dao;
+
+public abstract class AbstractCacheStore<K, V> implements CacheStore<K, V> {
+
+    protected abstract Dao<K, V> getBackingInMemoryDao();
+
+    protected abstract Dao<K, V> getBackingDataSourceDao();
+
+    @Override
+    public V getById(K key) {
+        return getBackingInMemoryDao().getById(key);
+    }
+
+    @Override
+    public V getOrFetchById(K key, boolean save) {
+        V byId = getBackingInMemoryDao().getById(key);
+
+        if(byId == null) {
+            byId = getBackingDataSourceDao().getById(key);
+
+            if(byId != null && save) {
+                put(key, byId, false);
+            }
+        }
+
+        return byId;
+    }
+
+    @Override
+    public void put(K key, V value, boolean save) {
+        getBackingInMemoryDao().save(key, value);
+
+        if(save) {
+            getBackingDataSourceDao().save(key, value);
+        }
+    }
+
+    @Override
+    public void update(K key, V value) {
+        getBackingInMemoryDao().update(key, value);
+        getBackingDataSourceDao().update(key, value);
+    }
+
+    @Override
+    public void remove(K key, boolean delete) {
+        getBackingInMemoryDao().delete(key);
+
+        if(delete) {
+            getBackingDataSourceDao().delete(key);
+        }
+    }
+
+}
---
+++ src/main/java/me/saiintbrisson/bukkit/boilerplate/common/AbstractInMemoryDao.java
@@ -0,0 +1,39 @@
+package me.saiintbrisson.bukkit.boilerplate.common;
+
+import com.google.common.cache.Cache;
+import me.saiintbrisson.bukkit.boilerplate.Dao;
+
+import java.util.Collection;
+
+public abstract class AbstractInMemoryDao<K, V> implements Dao<K, V> {
+
+    protected abstract Cache<K, V> getBackingCache();
+
+    @Override
+    public V getById(K key) {
+        return getBackingCache().getIfPresent(key);
+    }
+
+    @Override
+    public Collection<V> getAll() {
+        return getBackingCache().asMap().values();
+    }
+
+    @Override
+    public boolean save(K key, V element) {
+        getBackingCache().put(key, element);
+        return false;
+    }
+
+    @Override
+    public boolean update(K key, V element) {
+        return save(key, element);
+    }
+
+    @Override
+    public boolean delete(K key) {
+        getBackingCache().invalidate(key);
+        return false;
+    }
+
+}
